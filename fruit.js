f16b314a40a0895d8f30873089384a11b1b31aa9
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-10-28 12:46:22 +0530

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,46 @@
+img = "";
+status = "";
+object = [];
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Identification";
+
+            fill("#FF0000");
+            percent = floor(object[i].confidence * 100);
+            text(object[i].label + " " + percent + "%", object[i].x, object[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    staus = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+        object = results;
+    }
+
+    console.log(results);
+}
